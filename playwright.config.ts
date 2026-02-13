fe8f7bd77d257a0c8ecc9fbc74adc31873468d8d
Mary Dora <mary.d@iqzsystems.com>
2026-02-13 12:44:31 +0530

    Fixed headeless issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: process.env.CI ? true : false,
     screenshot: 'only-on-failure',
     video : 'retain-on-failure',
 
